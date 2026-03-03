60b19a76b4ad17d2f942dcc224bd6c9b1db155c1
bz <342520697@qq.com>
2026-03-03 15:30:50 +0800

    提交初始化代码

---
+++ src/main/java/com/qy/citytechupgrade/CityTechUpgradeApplication.java
@@ -0,0 +1,13 @@
+package com.qy.citytechupgrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityTechUpgradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityTechUpgradeApplication.class, args);
+    }
+
+}
